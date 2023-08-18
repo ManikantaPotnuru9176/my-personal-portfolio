44cc956b2f330de63479f86a3a2b0d1819e079a6
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-08-18 22:44:16 +0530

    optimized the navbar code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,58 @@
 import { useTheme } from "next-themes";
 import { Bio } from "../../constants";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const { theme } = useTheme();
 
-  const navigationLinks = [
-    { text: "About", href: "#about" },
-    { text: "Skills", href: "#skills" },
-    { text: "Experience", href: "#experience" },
-    { text: "Projects", href: "#projects" },
-    { text: "Education", href: "#education" },
-  ];
+  const navigationLinks = useMemo(
+    () => [
+      { text: "About", href: "#about" },
+      { text: "Skills", href: "#skills" },
+      { text: "Experience", href: "#experience" },
+      { text: "Projects", href: "#projects" },
+      { text: "Education", href: "#education" },
+    ],
+    []
+  );
 
   const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
+    setIsMobileMenuOpen((prevIsMobileMenuOpen) => !prevIsMobileMenuOpen);
   };
+
+  const primaryTextStyles = `text-primary dark:text-white hover:text-primary dark:hover:text-primary`;
+  const githubButtonStyles = `hover:bg-primary dark:hover:text-white transition duration-1000 font-semibold text-while dark:text-primary border-2 border-primary hidden md:flex items-center h-10 rounded-full px-4 ${primaryTextStyles}`;
+
+  const renderNavigationLinks = () =>
+    navigationLinks.map(({ text, href }) => (
+      <li key={text}>
+        <Link href={href}>
+          <span className={primaryTextStyles} onClick={toggleMobileMenu}>
+            {text}
+          </span>
+        </Link>
+      </li>
+    ));
 
   return (
     <nav className="bg-white dark:bg-backgroundNavbar px-6 py-8 ring-1 ring-slate-900/5 shadow-xl h-16 flex items-center justify-center font-semibold sticky top-0 z-10 transition duration-300 ease-in-out">
-      <div className="max-w-6xl mx-auto flex justify-between items-center w-full px-4">
+      <div className="max-w-6xl mx-auto flex justify-between items-center w-full px-6">
         <Link href="/">
           <span className="w-12 h-12 flex items-center justify-center border-2 border-primary rounded-full text-black dark:text-white transition duration-200">
             MP
           </span>
         </Link>
 
-        <ul className="hidden md:flex gap-8">
-          {navigationLinks.map((link) => (
-            <li key={link.text}>
-              <Link href={link.href}>
-                <span
-                  className={`text-black dark:text-white cursor-pointer transition duration-200 hover:text-primary dark:hover:text-primary`}
-                >
-                  {link.text}
-                </span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul className="hidden md:flex gap-8">{renderNavigationLinks()}</ul>
 
-        {/* GitHub profile link */}
         <a
           href={Bio.github}
           target="_blank"
           rel="noopener noreferrer"
-          className={`border-2 border-primary hidden md:flex items-center h-10 rounded-full px-4 hover:bg-primary hover:text-white transition duration-1000 font-semibold text-while dark:text-primary dark:hover:text-white`}
+          className={githubButtonStyles}
+          aria-label="GitHub Profile"
         >
           GitHub Profile
         </a>
@@ -55,6 +60,7 @@ const Navbar = () => {
         <button
           className={`ml-4 md:hidden border-2 border-primary rounded-full p-2 focus:outline-none`}
           onClick={toggleMobileMenu}
+          aria-label="Toggle Mobile Menu"
         >
           <svg
             className={`w-6 h-6 text-primary dark:text-white transition duration-200 hover:text-primary dark:hover:text-primary`}
@@ -76,23 +82,12 @@ const Navbar = () => {
       {isMobileMenuOpen && (
         <div className="md:hidden absolute top-16 right-0 w-full bg-card_light dark:bg-card_dark border-t-2 border-primary">
           <ul className="flex flex-col items-center gap-4 p-4">
-            {navigationLinks.map((link) => (
-              <li key={link.text}>
-                <Link href={link.href}>
-                  <span
-                    className={`text-black dark:text-white cursor-pointer transition duration-200 hover:text-primary dark:hover:text-primary`}
-                    onClick={toggleMobileMenu}
-                  >
-                    {link.text}
-                  </span>
-                </Link>
-              </li>
-            ))}
+            {renderNavigationLinks()}
             <a
               href={Bio.github}
               target="_blank"
               rel="noopener noreferrer"
-              className={`border-2 border-primary flex items-center h-10 rounded-full px-4 hover:bg-primary hover:text-white transition duration-1000 font-semibold text-while dark:text-primary dark:hover:text-white`}
+              className={`border-2 border-primary flex items-center h-10 rounded-full px-4 ${primaryTextStyles}`}
             >
               GitHub Profile
             </a>

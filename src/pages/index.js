f7ab5e26fd7853b9b0938e6fcb58ea1920071eea
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-08-19 15:57:09 +0530

    converted dark mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,16 @@ import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Education from "@/components/Education";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setTheme("dark");
+  }, []);
+
   return (
     <div className="bg-white dark:bg-backgroundNavbar">
       <Navbar />
